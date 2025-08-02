122b07b6ba90f999b14108351cae37d76d16aa2e
Roy Smart <roytsmart@gmail.com>
2025-08-02 00:19:20 -0600

    Added `esis.optics.PrimaryMirror` class. (#11)

--- esis/optics/abc.py
+++ esis/optics/abc.py
@@ -4,8 +4,10 @@ Abstract classes used in this module.
 
 from ._front_apertures import AbstractFrontAperture
 from ._central_obscurations import AbstractCentralObscuration
+from ._primary_mirrors import AbstractPrimaryMirror
 
 __all__ = [
     "AbstractFrontAperture",
     "AbstractCentralObscuration",
+    "AbstractPrimaryMirror",
 ]
