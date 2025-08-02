eb5dbdb5764c6aa1504dda7a7da2cc564f135fd1
Roy Smart <roytsmart@gmail.com>
2025-08-02 00:30:31 -0600

    Added `esis.optics.FieldStop` class.

--- esis/optics/__init__.py
+++ esis/optics/__init__.py
@@ -6,10 +6,12 @@ from . import abc
 from ._front_apertures import FrontAperture
 from ._central_obscurations import CentralObscuration
 from ._primary_mirrors import PrimaryMirror
+from ._field_stops import FieldStop
 
 __all__ = [
     "abc",
     "FrontAperture",
     "CentralObscuration",
     "PrimaryMirror",
+    "FieldStop",
 ]
--- esis/optics/abc.py
+++ esis/optics/abc.py
@@ -5,9 +5,11 @@ Abstract classes used in this module.
 from ._front_apertures import AbstractFrontAperture
 from ._central_obscurations import AbstractCentralObscuration
 from ._primary_mirrors import AbstractPrimaryMirror
+from ._field_stops import AbstractFieldStop
 
 __all__ = [
     "AbstractFrontAperture",
     "AbstractCentralObscuration",
     "AbstractPrimaryMirror",
+    "AbstractFieldStop",
 ]
