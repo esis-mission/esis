0f0d67721c556903d2031d65dde714922b83d5e2
Roy Smart <roytsmart@gmail.com>
2025-08-19 08:02:40 -0600

    Added `esis.flights.f2.optics.materials` module.

---
+++ esis/flights/f2/optics/materials/__init__.py
@@ -0,0 +1,11 @@
+"""Models of the multilayer coatings."""
+
+from ._materials import (
+    multilayer_AlSc,
+    multilayer_SiSc,
+)
+
+__all__ = [
+    "multilayer_AlSc",
+    "multilayer_SiSc",
+]
