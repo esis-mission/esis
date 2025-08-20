8da151f205c441d07d71673070bac7ea58d88499
Roy Smart <roytsmart@gmail.com>
2025-08-20 13:39:58 -0600

    Added `esis.data.Level_0` class. (#32)

---
+++ esis/data/_level_0/_level_0_test.py
@@ -0,0 +1,53 @@
+import pytest
+import numpy as np
+import named_arrays as na
+import msfc_ccd
+from msfc_ccd._images._tests.test_sensor_images import AbstractTestAbstractSensorData
+import esis
+
+
+@pytest.mark.parametrize(
+    argnames="a",
+    argvalues=[
+        esis.data.Level_0.from_fits(
+            path=msfc_ccd.samples.path_dark_esis1,
+            camera=msfc_ccd.Camera(),
+        ),
+        esis.data.Level_0.from_fits(
+            path=na.ScalarArray(
+                ndarray=np.array(
+                    [
+                        msfc_ccd.samples.path_dark_esis1,
+                        msfc_ccd.samples.path_dark_esis3,
+                    ]
+                ),
+                axes="channel",
+            ),
+            camera=msfc_ccd.Camera(),
+        ),
+        esis.data.Level_0.from_fits(
+            path=na.ScalarArray(
+                ndarray=np.array(
+                    [
+                        msfc_ccd.samples.path_dark_esis1,
+                        msfc_ccd.samples.path_fe55_esis1,
+                    ]
+                ),
+                axes="time",
+            ),
+            camera=msfc_ccd.Camera(),
+            timeline=esis.nsroc.Timeline(),
+        ),
+    ],
+)
+class TestLevel_0(
+    AbstractTestAbstractSensorData,
+):
+    def test_timeline(self, a: esis.data.Level_0):
+        result = a.timeline
+        if result is not None:
+            assert isinstance(result, esis.nsroc.Timeline)
+
+    def test_channel(self, a: esis.data.Level_0):
+        result = a.channel
+        assert isinstance(result, na.ScalarArray)
--- esis/nsroc/_timeline.py
+++ esis/nsroc/_timeline.py
@@ -17,65 +17,65 @@ class Timeline(
     All times are relative to the start time of the mission.
     """
 
-    timedelta_esis_start: u.Quantity
+    timedelta_esis_start: None | u.Quantity = None
     """The mission time at the start of the ESIS exposure sequence."""
 
-    timedelta_rail_release: u.Quantity
+    timedelta_rail_release: None | u.Quantity = None
     """The mission time when the vehicle clears the launch rail."""
 
-    timedelta_terrier_burnout: u.Quantity
+    timedelta_terrier_burnout: None | u.Quantity = None
     """The mission time of the Terrier first stage burnout."""
 
-    timedelta_blackbrant_ignition: u.Quantity
+    timedelta_blackbrant_ignition: None | u.Quantity = None
     """The mission time of the Black Brant second stage ignition."""
 
-    timedelta_canard_decouple: u.Quantity
+    timedelta_canard_decouple: None | u.Quantity = None
     """The mission time when the S-19 guidance system releases the canards."""
 
-    timedelta_blackbrant_burnout: u.Quantity
+    timedelta_blackbrant_burnout: None | u.Quantity = None
     """The mission time of the Black Brant second stage burnout."""
 
-    timedelta_despin: u.Quantity
+    timedelta_despin: None | u.Quantity = None
     """The mission time when the despin sequence is initiated."""
 
-    timedelta_payload_separation: u.Quantity
+    timedelta_payload_separation: None | u.Quantity = None
     """The mission time when the payload separates from the rest of the vehicle."""
 
-    timedelta_sparcs_enable: u.Quantity
+    timedelta_sparcs_enable: None | u.Quantity = None
     """The mission time when the SPARCS pointing system is enabled."""
 
-    timedelta_shutter_open: u.Quantity
+    timedelta_shutter_open: None | u.Quantity = None
     """The mission time when the payload shutter door is opened."""
 
-    timedelta_nosecone_eject: u.Quantity
+    timedelta_nosecone_eject: None | u.Quantity = None
     """The mission time when the nosecone is ejected from the top of the payload."""
 
-    timedelta_sparcs_finemode: u.Quantity
+    timedelta_sparcs_finemode: None | u.Quantity = None
     """The mission time when SPARCS is predicted to have acquired fine-pointing mode."""
 
-    timedelta_sparcs_rlg_enable: u.Quantity
+    timedelta_sparcs_rlg_enable: None | u.Quantity = None
     """The mission time when the ring-laser gyroscope is enabled."""
 
-    timedelta_sparcs_rlg_disable: u.Quantity
+    timedelta_sparcs_rlg_disable: None | u.Quantity = None
     """The mission time when the ring-laser gyroscope is disabled."""
 
-    timedelta_shutter_close: u.Quantity
+    timedelta_shutter_close: None | u.Quantity = None
     """The mission time when the payload shutter door is closed."""
 
-    timedelta_sparcs_spinup: u.Quantity
+    timedelta_sparcs_spinup: None | u.Quantity = None
     """The mission time when SPARCS spins up the payload for re-entry."""
 
-    timedelta_sparcs_vent: u.Quantity
+    timedelta_sparcs_vent: None | u.Quantity = None
     """The mission time when SPARCS vents leftover propellant."""
 
-    timedelta_ballistic_impact: u.Quantity
+    timedelta_ballistic_impact: None | u.Quantity = None
     """The predicted mission time of ballistic impact of the payload."""
 
-    timedelta_sparcs_disable: u.Quantity
+    timedelta_sparcs_disable: None | u.Quantity = None
     """The mission time when SPARCS is powered off."""
 
-    timedelta_parachute_deploy: u.Quantity
+    timedelta_parachute_deploy: None | u.Quantity = None
     """The mission time when the parachute is deployed."""
 
-    timedelta_payload_impact: u.Quantity
+    timedelta_payload_impact: None | u.Quantity = None
     """The mission time when the payload impacts the Earth on its parachute."""
