2362526d9c9189101335a7d33c8a2974df61fc3c
Roy Smart <roytsmart@gmail.com>
2025-08-24 23:14:20 -0600

    Added `esis.data.Level_0.lights` property.

--- esis/data/_level_0/_level_0_test.py
+++ esis/data/_level_0/_level_0_test.py
@@ -7,6 +7,12 @@ import msfc_ccd
 from msfc_ccd._images._tests.test_sensor_images import AbstractTestAbstractSensorData
 import esis
 
+_timeline = esis.nsroc.Timeline(
+    timedelta_esis_start=1 * u.s,
+    timedelta_sparcs_rlg_enable=60 * u.s,
+    timedelta_sparcs_rlg_disable=120 * u.s,
+)
+
 
 @pytest.mark.parametrize(
     argnames="a",
@@ -14,7 +20,7 @@ import esis
         esis.data.Level_0.from_fits(
             path=msfc_ccd.samples.path_dark_esis1,
             camera=msfc_ccd.Camera(),
-            timeline=esis.nsroc.Timeline(timedelta_esis_start=1 * u.s),
+            timeline=_timeline,
         ),
         esis.data.Level_0.from_fits(
             path=na.ScalarArray(
@@ -27,7 +33,7 @@ import esis
                 axes="channel",
             ),
             camera=msfc_ccd.Camera(),
-            timeline=esis.nsroc.Timeline(timedelta_esis_start=1 * u.s),
+            timeline=_timeline,
         ),
         esis.data.Level_0.from_fits(
             path=na.ScalarArray(
@@ -40,7 +46,7 @@ import esis
                 axes="time",
             ),
             camera=msfc_ccd.Camera(),
-            timeline=esis.nsroc.Timeline(timedelta_esis_start=1 * u.s),
+            timeline=_timeline,
         ),
     ],
 )
@@ -60,3 +66,8 @@ class TestLevel_0(
         result = a.time_mission_start
         assert isinstance(result, astropy.time.Time)
         assert result < a.inputs.time.ndarray.min()
+
+    def test_lights(self, a: esis.data.Level_0):
+        if a.axis_time in a.shape:
+            result = a.lights
+            assert isinstance(result, type(a))
