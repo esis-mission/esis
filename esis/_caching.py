a67c7cb61a946414cb20cd910d9584f6e8acdd9b
Roy Smart <roytsmart@gmail.com>
2025-09-07 13:54:47 -0600

    docs

--- esis/_caching.py
+++ esis/_caching.py
@@ -3,9 +3,5 @@ import joblib
 
 _path_cache = pathlib.Path(__file__).parent.parent / ".cache"
 
-memory = joblib.Memory(
-    location=_path_cache,
-    mmap_mode="r",
-    verbose=0,
-)
+memory = joblib.Memory(location=_path_cache, mmap_mode="r", verbose=0)
 """A representation of the cache which stores intermediate results."""
