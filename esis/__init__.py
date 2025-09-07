23f58800fda65907a10b683f5e7b8e0d1f023826
Roy Smart <roytsmart@gmail.com>
2025-09-07 15:28:02 -0600

    give up

--- esis/__init__.py
+++ esis/__init__.py
@@ -1,13 +1,12 @@
 """Model the ESIS optical system and invert images captured during flight."""
 
-from ._caching import foo, memory
+from ._caching import memory
 from . import optics
 from . import nsroc
 from . import data
 from . import flights
 
 __all__ = [
-    "foo",
     "memory",
     "optics",
     "nsroc",
--- esis/_caching.py
+++ esis/_caching.py
@@ -7,8 +7,5 @@ __all__ = [
 
 _path_cache = pathlib.Path(__file__).parent.parent / ".cache"
 
-foo = 5
-"""A test variable."""
-
 memory = joblib.Memory(location=_path_cache, mmap_mode="r", verbose=0)
 """A representation of the cache which stores intermediate results."""
