985483c4fa07238a28c0b370904c11d528f72925
Roy Smart <roytsmart@gmail.com>
2025-09-08 10:47:57 -0600

    Added `esis.flights.f1.optics.as_built()` model of the optical system. (#46)

--- esis/flights/f1/optics/_instruments/__init__.py
+++ esis/flights/f1/optics/_instruments/__init__.py
@@ -2,10 +2,12 @@ from ._instruments import (
     design_full,
     design,
     design_single,
+    as_built,
 )
 
 __all__ = [
     "design_full",
     "design",
     "design_single",
+    "as_built",
 ]
